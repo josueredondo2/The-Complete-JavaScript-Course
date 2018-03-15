5724c781bcb98a0d872ecc530e0a0bb4dfdb1a71
Josue <Josue.redondo2@gmail.com>
2018-03-14 19:58:28 -0600

    Initial commit

---
+++ JavaScript Language Basics/Practiques /Exercise 2.js
@@ -0,0 +1,11 @@
+var years = [1900, 2000, 2015];
+var emptyArray = [];
+
+for (var i = 0; i < years.length; i++) {
+	emptyArray[i] = years[i];
+}
+
+for (var a = 0; i < emptyArray.length; i++) {
+	console.log(emptyArray[a]);
+}
+
